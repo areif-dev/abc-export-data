153cc1ea9c7727ff74eeaf7187c55a909c7a6a73
areif-dev <aj@ajreifsnyder.com>
2025-06-29 20:19:36 -0400

    fix: export both inventory and customer files at once

--- src/abc-export/main.rs
+++ src/abc-export/main.rs
@@ -123,19 +123,17 @@ fn main() {
     println!("Attempting to login to ABC");
     abc_uiautomation::login(&abcwin, &config.abc_username, &config.abc_password)
         .expect("Failed to login to ABC");
-    println!("Attempting to export Item data");
+    println!("Attempting to export Item and Customer data");
     export_file(
         &abcwin,
-        "I",
+        "I,C",
         "C:/ABC Software/Database Export/Company001/Data",
-        &["item.data", "item_posted.data"],
-    );
-    println!("Attempting to export Customer data");
-    export_file(
-        &abcwin,
-        "C",
-        "C:/ABC Software/Database Export/Company001/Data",
-        &["customer.data", "customer_posted.data"],
+        &[
+            "item.data",
+            "item_posted.data",
+            "customer.data",
+            "customer_posted.data",
+        ],
     );
     println!("Success!");
 }
